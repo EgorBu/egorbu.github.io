81ec54df3b4c55f8e2fe2dacec81e4b5e5ba80bb
egor <egor@sourced.tech>
2019-09-15 00:55:10 +0200

    Langs, code to generate pictures, several fixes
    Signed-off-by: egor <egor@sourced.tech>

--- usedata_2019/pictures/what_mloncode/generate_lang_word_distr.py
+++ usedata_2019/pictures/what_mloncode/generate_lang_word_distr.py
@@ -1,213 +1,33 @@
+import os
+
 from matplotlib import pyplot as plt
 
-wiki = """
-https://en.wikipedia.org/wiki/List_of_dictionaries_by_number_of_words
+wiki_link = "https://en.wikipedia.org/wiki/List_of_dictionaries_by_number_of_words"
 
-Korean	1,100,373	
- 
-우리말샘 (Woori Mal Saem, 2017)	Online open dictionary including dialects of South and North Korea.[3]
-Finnish	800,000	
- 
-RedFox Pro	Online dictionary. The free version has over 300,000 Finnish words and the Pro version has over 800,000 Finnish words. The dictionary has agglomerated other dictionaries, such as technical ones[4], and the largest set comes from Wordnet[5]. Note that even this dictionary essentially doesn't include inflections.
-Turkish	616,767	
- 
-Büyük Türkçe Sözlük	Online dictionary of the Turkish Language Association[6]
-Swedish	600,000	
- 
-Svenska Akademiens ordbok, Swedish Academy	After having completed letters A through T SAOB included 470,000 words, but 600,000 words when the alphabet was completed in 2017. Svenska Akademiens ordlista, which includes only commonly used words, currently includes ~126,000 words after having added 13,500 and removed 9,000 in its latest edition, SAOL 14, plus an additional 200,000 still encountered words in earlier editions.[7][8]
-Icelandic	560,000	
- 
-Orðabók Háskólans	43,000 basic words and 519,000 compound words of which more than half are attested only once or don't get into print (“instant combinations”)[9]
-Japanese	500,000	
- 
-Nihon Kokugo Daijiten	[10]
-Lithuanian	500,000	
- 
-Lietuvių kalbos žodynas (Academic Dictionary of Lithuanian)	22,000 pages in 20 volumes with quotations from all kinds of writing and dialect records between 1547 and 2001. Accessible online at www.lkz.lt.[11]
-Norwegian	500,000	
- 
-Norsk Ordbok	[12]
-English	470,000	
- 
-Webster's Third New International Dictionary and Addenda Section	Contains 470,000 entries[13]
-Portuguese	442,000	
- 
-Novo Dicionario Houaiss da Lingua Portuguesa	The dictionary contains 442,000 entries, phrases and meanings.[14]
-Dutch	400,000	
- 
-Woordenboek der Nederlandsche Taal	The 43 volumes of the WNT (including three supplements) consist of 49,255 pages, describing Dutch words from 1500 to 1976.[15]
-Chinese	370,000	
- 
-Hanyu Da Cidian	[16]
-English	350,000	
- 
-The American Heritage Dictionary of the English Language, Third Edition	In the introduction to the 4th and 5th editions, it is mentioned that more than 10,000 words have been added, thus the total for the 5th edition will be more than 370,000 words.[17][failed verification]
-Finnish	350,000	
- 
-Suomen murteiden sanakirja (in progress)	Suomen murteiden sanakirja (SMS) will include 350,000 words from different dialects, with well-documented definitions, based on the archives (Suomen murteiden sana-arkisto) of 400,000 words, with over 8 million definitions.[18] [19]
-
-German	330,000	
- 
-Deutsches Wörterbuch	330,000 words in use since the mid-fifteenth century. — Duden's Großes Wörterbuch der deutschen Sprache contains over 200,000 contemporary words.[20][21]
-Norwegian	300,000	
- 
-Tanums store rettskrivningsordbok (10. utgave)	[22]
-Gujarati	281,377	
- 
-Bhagavadgomandal	2.81 lakh words and their meanings in 9 volumes. Also serves as an encyclopedia with almost 8.22 lakh words.[23]
-Italian	260,000	
- 
-Grande dizionario italiano dell'uso	The number of "sayable and writable" word forms is estimated at over 2 million[24]
-Czech	250,000	
- 
-cs:Příruční slovník jazyka českého	Nine volumes of this dictionary were printed in years 1935-1957. They contain about 250,000 words, their meanings and example usage from literature. The dictionary is available online.[25]
-Belarusian	223,000	
- 
-Большой словарь белорусского языка	[26]
-English	207,016	
- 
-WordNet, 3.1	As of November 2012 WordNet's latest Online-version is 3.1. The database contains 155,327 words organized in 175,979 synsets for a total of 207,016 word-sense pairs.[27]
-Finnish	201,000	
- 
-Nykysuomen sanakirja, 1961	Nykysuomen sanakirja can be translated to The Dictionary of Modern Finnish or The Dictionary of Contemporary Finnish, but the language can be quite dated; the dictionary only reflects the language as it was no later than 1961. Even though it has been published again, it hasn not been updated. The dictionary contains over 201,000 headwords in six volumes.[28] For modern language, The New Dictionary of Modern Finnish is more relevant.
-Danish	200,000	
- 
-Ordbog over det danske sprog, Dansk Sprognævn	Dansk Sprognævn grows with 5,000 to 7,000 words a year[29]
-Slovak	200,000	
- 
-Slovník slovenského jazyka z r. 1959 – 1968, Slovník súčasného slovenského jazyka A – G, H – L, M – N z r. 2006, 2011, 2015	Here is the information about the number of words in Slovak language written by Jazykovedný ústav Ľ. Štúra SAV.
-
-Romanian	180,000	
- 
-dexonline	Online dictionary. Project of digitisation of 67 general, specialty and archaic dictionaries. Launched in 2001. As of 2013, it contained over 180,000 unique words and 576,000 definitions.
-English	171,476	
- 
-Oxford English Dictionary, Second Edition	Oxford Dictionary has 273,000 headwords; 171,476 of them being in current use, 47,156 being obsolete words and around 9,500 derivative words included as subentries. The dictionary contains 157,000 combinations and derivatives in bold type, and 169,000 phrases and combinations in bold italic type, making a total of over 600,000 word-forms.[30][31]
-There is one count that puts the English vocabulary at about 1 million words — but that count presumably includes words such as Latin species names, prefixed and suffixed words, scientific terminology, jargon, foreign words of extremely limited English use and technical acronyms.[32][33][34]
-Russian	150,000	
- 
-Большой академический словарь русского языка	Great Academy Dictionary of Russian language[35]
-Belarusian	150,000	
- 
-Слоўнік беларускай мовы	[36]
-French	135,000	
- 
-Trésor de la Langue Française informatisé	ATILF[37] (Analyse et Traitement Informatique de la Langue Française – Computer Processing and Analysis of the French Language)
-135,000 (Larousse Dictionnaire de français, published by Editions Larousse)[38][39]
-Ukrainian	134,058	
- 
-Словник української мови (The Dictionary of the Ukrainian language)	The dictionary was finished in late 1970s - early 1980s[40][41]
-Russian	130,000	
- 
-Большой толковый словарь русского языка	Great Dictionary of Russian language[42]
-Indonesian	127,036	
- 
-Kamus Besar Bahasa Indonesia, 5th edition, 2016	
-Tamil	124,405	
- 
-University of Madras Tamil Lexicon	The dictionary includes 124,405 separate entries.[43]
-Arabic	120,000	
- 
-Taj al-Arus Min Jawahir al-Qamus	
-Bulgarian	119,200	
- 
-Dictionary of the Bulgarian Language (monolingual academic explanatory dictionary), (Многотомен) Речник на българския език in Bulgarian, in 15+ volumes	This dictionary covers vocabulary from the last 150 years of the Bulgarian language and is compiled and edited by linguistics (primarily native lexicographers and lexicologists) from The Institute for the Bulgarian Language (part of the Bulgarian Academy of Sciences). It includes basic, commonly used, literary, colloquial, dialectical, archaic and obsolete Bulgarian words, as well as some specialized terminology. The latest volume (15th) published in 2015 ends with headwords beginning with the (Bulgarian Cyrillic) letter Р.[44]
-Belarusian	112,462	
- 
-Skarnik	As of August 2019. Belarusian-Russian online dictionary contains 112,462 words.[45]
-Slovene	110,180	
- 
-Slovar slovenskega knjižnega jezika, Second edition, 2014	The official dictionary of modern Slovene is Slovar slovenskega knjižnega jezika (SSKJ; Standard Slovene Dictionary). It was published in five volumes by Državna Založba Slovenije between 1970 and 1991 and contains more than 100,000 entries and subentries with accentuation, part-of-speech labels, common collocations, and various qualifiers. In the 1990s, an electronic version of the dictionary was published and it is available online.[46]
-Finnish	102,174	
- 
-Kielitoimiston sanakirja, 2018	Online dictionary. Institute for the Languages of Finland (governmental institute) has selected the core vocabulary, and many headwords are not included.[47]
-
-Polish	100,000	
- 
-Słownik języka polskiego PWN	Polish dictionary of PWN contains about 100,000 articles and 145,000 definitions.[48]
-French	100,000	
- 
-Dictionnaire Le Grand Robert de la langue française, 2019	Contains 100,000 words and 350,000 definitions.[49]
-Spanish	93,000	
- 
-Diccionario de la lengua española de la Real Academia Española, 23rd edition, 2014	[50]
-Dutch	90,000	
- 
-Van Dale, 14th edition, 2005	[51]
-Chinese	85,568	
- 
-Zhonghua Zihai	Number of different characters in use over three millennia of written history. The Hanyu Da Cidian defines some 370,000 words.[52][53][54]
-
-Malaysian	82,900	
- 
-Kamus Dewan, 4th Edition, 2005	
-Persian	80,000	
- 
-Farhang-e bozorg-e Sokan, 2002	[55]
-Chechen	70,000	
- 
-Словарь Чеченского языка	
-Catalan	69,988	
- 
-Diccionari de la llengua catalana (Dictionary of the Catalan language of the Institute of Catalan Studies (DIEC))
-The dictionary includes 69,988 headwords and 132,343 definitions.[56]
-Romanian	67,000	
- 
-Dicționarul explicativ al limbii române (Published by the Romanian Academy)	
-Galician	59,999	
- 
-Dicionario da Real Academia Galega (Dictionary of the Royal Galician Academy)	[57]
-Tatar	56,000	
- 
-Татарско-русский словарь Ш.Н. Асылгараева, Ф.А. Ганиева, М.З. Закиева, К.М. Миннуллина, Д.Б. Рамазанова	Tatar-Russian dictionary of Sh.N. Asylgaraev, F.A. Ganiev, M.Z. Zakiyev, K.M. Minnullin, D.B. Ramazanova[58]
-Turkmen	50,000	
- 
-Türkmen diliniň düşündirişli sözlügi	Turkmen Explanatory Dictionary[59]
-Azerbaijani	44,750	
- 
-Azərbaycan dilinin izahlı lüğəti	Azerbaijani Explanatory Dictionary[60]
-Bashkir	40,000	
- 
-Башкирско-русский словарь Ураксин З.Г.	Bashkir-Russian dictionary Uraksin Z. G.[61]
-Chuvash	40,000	
- 
-Чувашско-русский словарь Скворцова М. И.	Chuvash-Russian dictionary Skvortsova M. I.[62]
-Dargwa	40,000	
- 
-Даргинско-русский словарь Юсупова Х. А.	Dargwa-Russian dictionary of Yusupov H. A[63]
-Classical Latin	39,589	
- 
-Oxford Latin Dictionary	This dictionary includes 39,589 Classical Latin entries, including those borrowed from Greek, Gaulish, other Italic dialects, Sanskrit, as well as others. There are approximately: 10,000 Adjectives, 2,123 Adverbs, 46 Conjunctions, 77 Interjections, 17450 Nouns, 26 Particles, 39 Prepositions, 17 Pronouns, and 5,986 verbs. The remaining entries are references to other entries (such as alternate spellings or archaic versions), prefixes, suffixes, and terms left untranslated by the original editors.[64]
-Avar	36,000	
- 
-Аварско-русский словарь Гимбатова. М. М.	Avar-Russian dictionary of Gimbatov M. M
-Arabic	32,300	
- 
-Lexicon of the Modern Arabic Language	
-Quechua	20,000	
- 
-Diccionario Quechua-Español Lira Jorge	Quechua-Spanish dictionary Lira Jorge [65]
-Esperanto	16,780	
- 
-Plena Ilustrita Vortaro de Esperanto (Complete Illustrated Dictionary of Esperanto)	46,890 lexical units[66]
-"""
-
-lang_word_cnt = []
+root = "../why_mloncode_hard/langs/"
 
 cnts = [49 * 10 ** 6]
-languages = ["source code identifiers"]
+languages = ["Source Code Identifiers"]
+
+# cnts = []
+# languages = []
 
 data_to_use = """Korean	1,100,373
-Finnish	800,000
 Turkish	616,767
-Swedish	600,000
-Icelandic	560,000
 Japanese	500,000
-Lithuanian	500,000
-Norwegian	500,000
 English	470,000
 Russian	150,000
 """
+
+
+def human_format(num):
+    magnitude = 0
+    while abs(num) >= 1000:
+        magnitude += 1
+        num /= 1000.0
+    # add more suffixes if you need them
+    return '%.0f%s' % (num, ['', 'K', 'M', 'G', 'T', 'P'][magnitude])
+
 
 for line in data_to_use.splitlines():
     words = line.strip().split()
@@ -216,22 +36,94 @@ for line in data_to_use.splitlines():
         cnts.append(int(words[1].replace(",", "")))
         languages.append(words[0])
 
-fig, ax = plt.subplots()
-print(cnts, languages)
-bar_plot = plt.bar(x=range(len(cnts)), height=cnts, tick_label=languages, log=True,
-                   width=.9999)
+languages = list(reversed(languages))
+cnts = list(reversed(cnts))
 
 
-def autolabel(rects, ax, bar_labels):
+def plot_hist(langs, cnts, transparent_since=-1):
+    return plt.barh(y=range(len(cnts)), width=cnts, tick_label=[""] * len(cnts),
+                    height=.9999, fc=(1, 0, 0, 0.5), edgecolor='black', hatch="/")
+
+
+def autolabel(rects, ax, bar_labels, transparent_since=-1, show_last=False):
+    # import pdb;pdb.set_trace()
+    x_step = max(rect.get_width() for rect in rects) * 0.1
     for idx, rect in enumerate(rects):
         height = rect.get_height()
-        ax.text(rect.get_x() + rect.get_width() / 2., 1.05 * height,
-                bar_labels[idx],
-                ha='center', va='bottom', rotation=90)
+        rect_text = bar_labels[idx]
+        if idx < transparent_since:
+            rect_text += " " + human_format(rect.get_width())
+        ax.text(rect.get_x() + x_step, rect.get_y() + .5 * height,
+                rect_text,
+                ha='left', va='center', rotation=0, weight='bold', fontsize=38)
 
-autolabel(rects=bar_plot, ax=ax, bar_labels=languages
-          )
-plt.show()
+        if idx >= transparent_since:
+            rect.set_alpha(0)
+            continue
+        rect.set_linewidth(10)
+
+        " + rect.get_width() / 2."
+        print(rect.get_width(), rect.get_x())
+    if show_last:
+        rect_text = bar_labels[-1]
+        rect_text += " " + human_format(rects[-1].get_width())
+        height = rects[-1].get_height()
+        ax.text(rects[-1].get_x() + x_step, rects[-1].get_y() + .5 * height,
+                rect_text,
+                ha='left', va='center', rotation=0, weight='bold', fontsize=38)
+        rects[-1].set_linewidth(10)
+
+
+def plot_pipeline(langs, cnts):
+    # plt.ion()
+    for i in range(len(cnts)):
+        tmp_langs = [l for l in langs[:i + 1]]
+        tmp_cnts = [c for c in cnts[:i + 1]]
+        max_v = tmp_cnts[-1]
+
+        for _ in range(len(cnts) - i - 1):
+            tmp_langs.append("")
+            tmp_cnts.append(max_v * 2)
+
+        fig, ax = plt.subplots()
+        fig.set_size_inches(12, 12)
+        bar_plot = plot_hist(langs=tmp_langs, cnts=tmp_cnts, transparent_since=i)
+
+        autolabel(rects=bar_plot, ax=ax, bar_labels=tmp_langs, transparent_since=i)
+        ax.xaxis.set_visible(False)
+        ax.yaxis.set_visible(False)
+        ax.spines['right'].set_visible(False)
+        ax.spines['top'].set_visible(False)
+        ax.spines['bottom'].set_visible(False)
+        plt.show(block=False)
+        plt.savefig(os.path.join(root, "langs_%s.png" % i))
+    fig, ax = plt.subplots()
+    fig.set_size_inches(12, 12)
+    bar_plot = plot_hist(langs=langs, cnts=cnts, transparent_since=len(cnts))
+
+    autolabel(rects=bar_plot, ax=ax, bar_labels=langs, transparent_since=len(cnts), show_last=True)
+    ax.xaxis.set_visible(False)
+    ax.yaxis.set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+    plt.show(block=False)
+    plt.savefig(os.path.join(root, "langs_%s.png" % len(cnts)))
+    plt.show()
+
+
+plot_pipeline(langs=languages, cnts=cnts)
+
+# fig, ax = plt.subplots()
+# "tick_label=languages, log=True"
+# bar_plot = plt.barh(y=range(len(cnts)), width=cnts, tick_label=[""] * len(cnts),
+#                     height=.9999, fc=(1, 0, 0, 0.5),  edgecolor='black', hatch="/")
+#
+#
+#
+#
+# autolabel(rects=bar_plot, ax=ax, bar_labels=languages)
+# plt.show()
 
 
 
